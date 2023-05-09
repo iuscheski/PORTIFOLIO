1999b6dba3f1a24f5309dc2d13e5fbe94f2a8975
Felipe Almeida <74160017+iuscheski@users.noreply.github.com>
2023-05-09 11:36:12 -0300

    UPDATE-CONTACT

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   rotulos.classList.toggle("active");
 });
+
+document.querySelectorAll(".rotulos-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    rotulos.classList.remove("active");
+  })
+);
+
 function App() {}
 
 export default App;
