fce9426687a3188103cfe064530c25551449d604
Felipe Almeida <74160017+iuscheski@users.noreply.github.com>
2023-05-09 15:54:40 -0300

    Touch-update

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   rotulos.classList.toggle("active");
 });
-
-document.querySelectorAll(".rotulos-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    rotulos.classList.remove("active");
-  })
-);
-
 function App() {}
 
 export default App;
