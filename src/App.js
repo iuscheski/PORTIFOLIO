65fec8e9686d88e8ea3df891279c8753792652c4
Felipe Almeida <74160017+iuscheski@users.noreply.github.com>
2023-05-10 17:49:37 -0300

    PRE-FINAL-VERISON-1.0

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ const hamburger = document.querySelector(".hamburger");
 const rotulos = document.querySelector(".rotulos");
 const enhamburger = document.querySelector(".en-hamburger");
 const enrotulos = document.querySelector(".en-rotulos");
+const frhamburger = document.querySelector(".fr-hamburger");
+const frrotulos = document.querySelector(".fr-rotulos");
 
 document.addEventListener("touchstart", function () {}, true);
 hamburger.addEventListener("click", () => {
@@ -13,6 +15,11 @@ document.addEventListener("touchstart", function () {}, true);
 enhamburger.addEventListener("click", () => {
   enhamburger.classList.toggle("active");
   enrotulos.classList.toggle("active");
+});
+document.addEventListener("touchstart", function () {}, true);
+frhamburger.addEventListener("click", () => {
+  frhamburger.classList.toggle("active");
+  frrotulos.classList.toggle("active");
 });
 function App() {}
 
